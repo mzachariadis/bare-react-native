357d7017ddd33a455e27787fb6c285143102c93c
Michalis Zachariadis <zachmich@gmail.com>
2021-02-01 00:09:50 +0200

    feat: enhance login & home screens

--- src/components/screens/HomeScreen.js
+++ src/components/screens/HomeScreen.js
@@ -1,14 +1,39 @@
-import React from 'react';
-import {SafeAreaView, View, Text} from 'react-native';
+import React, {useContext} from 'react';
+import {Button, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import AuthContext from '../../context/AuthContext';
 
 function HomeScreen() {
+  const {user, handleLogout} = useContext(AuthContext);
+
+  function handleClickLogout() {
+    handleLogout();
+  }
+
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Home screen</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.textWrapper}>
+        <Text>Welcome {user.username}</Text>
+      </View>
+      <View style={styles.buttonWrapper}>
+        <Button title="Logout" onPress={handleClickLogout} />
       </View>
     </SafeAreaView>
   );
 }
 
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  textWrapper: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonWrapper: {
+    flex: 2,
+    alignItems: 'center',
+  },
+});
--- src/components/screens/LoginScreen.js
+++ src/components/screens/LoginScreen.js
@@ -1,14 +1,43 @@
-import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import React, {useContext} from 'react';
+import {Button, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import AuthContext from '../../context/AuthContext';
 
 function LoginScreen() {
+  const {handleLogin} = useContext(AuthContext);
+
+  function handleClickLogin() {
+    handleLogin({
+      user: {
+        username: 'mzachariadis',
+      },
+    });
+  }
+
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Login screen</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.textWrapper}>
+        <Text>Welcome unknown user</Text>
+      </View>
+      <View style={styles.buttonWrapper}>
+        <Button title="Login" onPress={handleClickLogin} />
       </View>
     </SafeAreaView>
   );
 }
 
 export default LoginScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  textWrapper: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonWrapper: {
+    flex: 2,
+    alignItems: 'center',
+  },
+});
